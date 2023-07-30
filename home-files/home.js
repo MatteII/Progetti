feb4552152a861ad52fbb174cf1873d7b52277f3
Matte <matteo.desandre01@gmail.com>
2023-07-31 00:32:17 +0200

    home files

---
+++ home-files/home.js
@@ -0,0 +1,14 @@
+/*
+
+
+$(function() {
+  $('.card_link').hover(function() {
+    $('.card_outer').css('background-color', 'yellow');
+  }, function() {
+    // on mouseout, reset the background colour
+    $('.card_outer').css('background-color', '');
+  });
+});
+
+
+*/
