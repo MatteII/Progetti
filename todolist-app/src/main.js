ddaa3a6d486613ec1374bd568061cfafa5e8b8b6
Matte <136202614+MatteII@users.noreply.github.com>
2023-08-05 23:57:57 +0200

    Update main.js

--- todolist-app/src/main.js
+++ todolist-app/src/main.js
@@ -1,4 +1,4 @@
-import { Vue, createApp } from 'vue'
+import {createApp} from 'vue/dist/vue.esm-bundler.js';
 import '/Progetti/todolist-app/src/main.css'
 import App from '/Progetti/todolist-app/src/App.vue'
 
