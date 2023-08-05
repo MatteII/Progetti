7a3336257d56ae9a71f096cda89a549f874b50fc
Matte <136202614+MatteII@users.noreply.github.com>
2023-08-06 00:05:38 +0200

    Update main.js

--- todolist-app/src/main.js
+++ todolist-app/src/main.js
@@ -1,5 +1,13 @@
-import {createApp} from '/vue';
-import '/Progetti/todolist-app/src/main.css'
-import App from '/Progetti/todolist-app/src/App.vue'
+//import {createApp} from '/vue';
+//import '/Progetti/todolist-app/src/main.css'
+//import App from '/Progetti/todolist-app/src/App.vue'
 
-createApp(App).mount('#app')
+//createApp(App).mount('#app')
+
+
+
+import { createApp } from 'vue'
+import App from '/Progetti/todolist-app/src/App.vue'
+import '/Progetti/todolist-app/src/main.css'
+const app = createApp(App)
+app.mount('#app')
