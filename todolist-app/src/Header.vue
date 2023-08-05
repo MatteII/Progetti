bf3e26c791dc4529436bc78ec7ee639578ba1e99
Matte <136202614+MatteII@users.noreply.github.com>
2023-08-05 23:36:57 +0200

    Update Header.vue

--- todolist-app/src/Header.vue
+++ todolist-app/src/Header.vue
@@ -2,7 +2,7 @@
     <header>
         <div class="container">
             <div class="header-flex">
-                <img src="/src/assets/img/LogoMDS_White.png" alt="Logo" />
+                <img src="/Progetti/todolist-app/src/assets/img/LogoMDS_White.png" alt="Logo" />
                 <h1>Matteo De Sandre</h1>
                 <span>ToDo App</span>
             </div>
